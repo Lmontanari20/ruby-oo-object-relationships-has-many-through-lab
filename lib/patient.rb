271856603c2fe2ec56e8a3e96ad6f3662dc3fb5d
Lucas Montanari <Lmonty2011@gmail.com>
2020-11-22 12:19:20 -0800

    implemented appointment, artist, doctor, genre, patient and song classes

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,28 @@
+require 'pry'
+class Patient
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        #binding.pry
+        Appointment.all.select { |app| app.patient == self}
+    end
+
+    def doctors
+        self.appointments.map { |app| app.doctor}
+    end
+end
